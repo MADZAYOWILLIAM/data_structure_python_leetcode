650fc6f0f8d39942141f84d182ebb624dadd39d6
MADZAYO_WILLIAM <antoniechiko@gmail.com>
2025-11-13 13:34:01 +0300

    String Merging

--- Basic/hello.py
+++ Basic/hello.py
@@ -1,1 +1,20 @@
-print('Heloo William')
+"""STRING MERGING"""
+class Solution:
+    """Merge two strings by alternating characters from each."""
+    def mergeAlternately(slef,first_word:str,second_word:str):
+        merged_output=[]
+        min_len_word=min(len(first_word),len(second_word))
+
+        for i  in range(min_len_word):
+            merged_output.append(first_word[i])
+            merged_output.append(second_word[i])
+
+        if len(first_word)>len(second_word):
+            merged_output.append(first_word[min_len_word:])
+        elif len(second_word)>len(first_word):
+            merged_output.append(second_word[min_len_word:])
+
+        return ''.join(merged_output)
+
+        
+
